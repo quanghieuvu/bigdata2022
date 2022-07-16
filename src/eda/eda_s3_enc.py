ad0282a3700f597e12debe42f213a9cb6c7bb87c
Quang Hieu Vu <hieuvq@gmail.com>
2022-07-17 06:50:23 +0700

    analyze distribution of byte values in binary files

--- src/eda/eda_s3_enc.py
+++ src/eda/eda_s3_enc.py
@@ -1,12 +1,25 @@
 import os
 import glob
 
+from collections import Counter
+
 '''
 analyze the structure of enc binary files of S3
+ending file with b''
+equal distribution of byte values from 0 to 255
 '''
 def analyze_s3_enc_files():
     for filename in glob.glob(os.path.join("input", "S3", "train", "enc", "*")):
         print(filename)
+        bytes = []
+        with open(filename, "rb") as f:
+            byte = f.read(1)
+            while byte:
+                byte = f.read(1)
+                if byte != b'':
+                    bytes.append(byte)
+        c = Counter(bytes)
+        print(len(bytes), len(c), c)
 
 if __name__ == "__main__":
     analyze_s3_enc_files()
